919029aca4b221e8f8c16e81475c4febad2484ad
ashen-nextiom <ashen@nextiom.com>
2026-06-15 21:13:38 +0530

    update

--- nextiom-clean/nextiom/src/pages/RegisterPage.jsx
+++ nextiom-clean/nextiom/src/pages/RegisterPage.jsx
@@ -292,6 +292,7 @@ function RegisterPage() {
                   )
                 )}
               </div>
+              
 
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="space-y-2">
